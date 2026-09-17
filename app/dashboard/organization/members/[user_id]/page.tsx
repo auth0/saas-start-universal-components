09fe347c3bb630157134df655a62f37c947625e0
rax7389 <rahul12bhowmick@gmail.com>
2026-09-17 17:34:00 +0530

    feat: mm ea release changes

--- app/dashboard/organization/members/[user_id]/page.tsx
+++ app/dashboard/organization/members/[user_id]/page.tsx
@@ -1,17 +1,20 @@
 "use client"
 
-import { useParams, useRouter } from "next/navigation"
-import { OrganizationMemberDetail } from "@auth0/universal-components-react"
+import { useParams, useRouter, useSearchParams } from "next/navigation"
+import { OrganizationMemberDetail, OrganizationMemberDetailTab } from "@auth0/universal-components-react"
 
 export default function MemberDetailPage() {
   const router = useRouter()
   const params = useParams()
   const user_id = decodeURIComponent(params.user_id as string)
+  const searchParams = useSearchParams();
+  const tab = searchParams.get('tab') as OrganizationMemberDetailTab;
 
   return (
     <div className="p-6 pt-8">
       <OrganizationMemberDetail
         userId={user_id}
+        initialTab={tab}
         onBack={() => router.push("/dashboard/organization/members")}
       />
     </div>
